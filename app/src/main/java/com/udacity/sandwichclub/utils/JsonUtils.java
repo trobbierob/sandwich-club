23144a3255a20829e6c1a1b80998c4903c3e49a3
trobbierob <troberts@my.hpu.edu>
2018-04-26 18:44:25 -0400

    Finish JSON Parse

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -8,6 +8,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     private static final String TAG = JsonUtils.class.getSimpleName();
@@ -15,41 +18,56 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
 
         try {
+
+            //Parse Root Object
             JSONObject jsonRootObject = new JSONObject(json);
             Log.i(TAG, "JSON Object is... " + jsonRootObject);
 
+            //Parse Data Object
             JSONObject jsonDataObject = jsonRootObject.optJSONObject("name");
             Log.i(TAG, "JSON Object is... " + jsonDataObject);
 
-            String name = jsonDataObject.getString("mainName");
-            Log.i(TAG, "Sandwich name is... " + name);
+            //Parse Name of Sandwich
+            String mainName = jsonDataObject.getString("mainName");
+            Log.i(TAG, "Sandwich name is... " + mainName);
 
+            //Parse alsoKnowAs names of Sandwich
             JSONArray akaArray = jsonDataObject.optJSONArray("alsoKnownAs");
             Log.i(TAG, "Sandwich also name is... " + akaArray);
+            List<String> alsoKnownAs = new ArrayList<String>();
+            for(int i = 0; i < akaArray.length(); i++) {
+                alsoKnownAs.add(akaArray.get(i).toString());
+            }
+            Log.i(TAG, "List also name is... " + alsoKnownAs);
 
-
-
+            //Parse Place of Origin of Sandwich
             String placeOfOrigin = jsonRootObject.getString("placeOfOrigin");
             Log.i(TAG, "Place of origin is... " + placeOfOrigin);
 
-            String sandwichDescription = jsonRootObject.getString("description");
-            Log.i(TAG, "Description is... " + sandwichDescription);
+            //Parse Description of Sandwich
+            String description = jsonRootObject.getString("description");
+            Log.i(TAG, "Description is... " + description);
 
+            //Parse Image String
             String image = jsonRootObject.getString("image");
             Log.i(TAG, "Image String is... " + image);
 
-            JSONArray ingredients = jsonDataObject.optJSONArray("ingredients");
+            //Parse Ingredients of Sandwich
+            JSONArray ingredients = jsonRootObject.optJSONArray("ingredients");
             Log.i(TAG, "Sandwich also name is... " + ingredients);
+            List<String> ingredientsList = new ArrayList<String>();
+            for(int i = 0; i < ingredients.length(); i++) {
+                ingredientsList.add(ingredients.get(i).toString());
+            }
+            Log.i(TAG, "List also name is... " + ingredientsList);
 
-
+            //Return Sandwich
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredientsList);
 
         } catch (JSONException e){
             Log.e(TAG, "JSONException at " + e);
         }
-
-
-        //Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin, String description, String image, List<String> ingredients)
-
+        
         return null;
     }
 }
